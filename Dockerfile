596c701236c879f31feae2892aacfc9955b500f6
yukinomiu <yukinomiu@gmail.com>
2022-06-17 16:46:42 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:alpine3.16
+
+RUN apk add --no-cache --virtual .build-deps \
+    gcc \
+    linux-headers \
+    libc-dev \
+    libffi-dev
+
+RUN pip install --no-cache-dir \
+    libtorrent==2.0.6 && \
+    deluge==2.0.5
+
+RUN apk del .build-deps && rm -rf /tmp
+
+ENV UMASK=022 \
+    LOG_LEVEL=warning
+
+COPY GeoIP.dat /usr/share/GeoIP/GeoIP.dat
+COPY run.sh /run.sh
+
+EXPOSE 8080 20000
+
+ENTRYPOINT ["sh", "/run.sh"]
---
+++ run.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+echo "==== START ===="
+
+# umask
+echo ">>> set umask..."
+umask=$UMASK
+umask $umask
+if [ $? -ne 0 ]; then
+    echo "umask set failed, please check the umask value $umask, exit"
+    exit 1
+else
+    echo "umask set to $umask"
+fi
+echo ">>> set umask success"
+
+# log level
+echo ">>> set deluge log level..."
+loglevel=$LOG_LEVEL
+echo "log level set to $loglevel"
+echo ">>> set deluge log level success"
+
+# prepare
+echo ">>> prepare files..."
+mkdir -p /log
+mkdir -p /config
+mkdir -p /downloads
+echo ">>> prepare files success"
+
+# deluge-web
+echo ">> start deluge web interface..."
+echo "deluge-web version:"
+/usr/local/bin/deluge-web --version
+/usr/local/bin/deluge-web -l /log/web.log -L $loglevel --logrotate 1M -p 8080 --config /config
+echo ">> start deluge web interface success"
+
+# deluged
+echo ">>> start deluge daemon..."
+echo "deluged version:"
+/usr/local/bin/deluged --version
+/usr/local/bin/deluged -d -l /log/daemon.log -L $loglevel --logrotate 1M --config /config
+echo ">>> start deluge success"
+
+echo "==== END ===="
